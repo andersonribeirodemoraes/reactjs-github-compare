e27d0ce2130c14daa6e3bbafee4f578813e4511f
Anderson Ribeiro <anderson.moraes@m2msolutions.com.br>
2019-05-30 20:14:02 -0300

    commit inicial

---
+++ src/pages/Main.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import api from '../services/api';
+
+import Logo from '../assets/logo.png';
+
+import { Container, Form } from './styles';
+import CompareList from '../components/CompareList';
+
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
+
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    try {
+      this.setState({ loading: true });
+      const { repositoryInput, repositories } = this.state;
+
+      const { data: repository } = await api.get(`repos/${repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryError: false,
+        repositoryInput: '',
+        repositories: [...repositories, repository],
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const {
+      repositoryError, repositoryInput, loading, repositories,
+    } = this.state;
+    return (
+      <Container>
+        <img src={Logo} alt="Giuhub Compare" />
+
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuaŕio/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">{loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}</button>
+        </Form>
+
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
